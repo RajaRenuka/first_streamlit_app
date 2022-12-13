b2b61f99c50e38b669ce2109d58023dead55a158
Raja Renuka <120446337+RajaRenuka@users.noreply.github.com>
2022-12-13 12:56:36 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 streamlit.title("This is my first hello world in github")
 streamlit.title(" i am going through github and streamlit , many more things i am learening now")
+Streamlit.header("Snowflake")
+streamlist.text("a. warehouse")
+streamlist.text("b.database")
+streamlist.text("c.schema")
